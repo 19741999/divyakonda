a787ed66c1b71e866ddf8be13a8e7d8e7680e400
19741999 <47383269+19741999@users.noreply.github.com>
2026-09-21 01:56:20 +0530

    Update main.tf

--- modules/catalog/main.tf
+++ modules/catalog/main.tf
@@ -1,25 +1,12 @@
-resource "databricks_storage_credential" "this" {
+resource "databricks_storage_credential" "name" {
   name = local.storage_credential_name
-
   aws_iam_role {
     role_arn = var.iam_role_arn
   }
-
-  comment = "RWD storage credential for s3://${var.bucket_name}"
 }
 
-resource "databricks_catalog" "this" {
+resource "databricks_catalog" "name" {
   name         = local.catalog_name
   storage_root = "s3://${var.bucket_name}/"
-  comment      = "Catalog backed by s3://${var.bucket_name}/"
-
   depends_on = [databricks_storage_credential.this]
-}
-
-output "catalog_name" {
-  value = databricks_catalog.this.name
-}
-
-output "storage_credential_name" {
-  value = databricks_storage_credential.this.name
 }
