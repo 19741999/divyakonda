e2525d19a6eaaaf97c90515b8cd389f4fb5bd75c
19741999 <47383269+19741999@users.noreply.github.com>
2026-09-21 02:03:27 +0530

    Update main.tf

--- modules/external-locations/main.tf
+++ modules/external-locations/main.tf
@@ -16,15 +16,7 @@ resource "databricks_external_location" "this" {
   name            = each.value.name
   url             = each.value.url
   credential_name = var.storage_credential_name
-  comment         = "External location for ${each.key}"
+
+
 }
 
-output "location_names" {
-  description = "Map of 'stage_domain' => external location name"
-  value       = { for k, v in databricks_external_location.this : k => v.name }
-}
-
-output "location_urls" {
-  description = "Map of 'stage_domain' => external location S3 URL"
-  value       = { for k, v in databricks_external_location.this : k => v.url }
-}
