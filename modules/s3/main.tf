af18a45ad8f9f29c4baf3e106851103647d8dffe
19741999 <47383269+19741999@users.noreply.github.com>
2026-09-21 02:06:14 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "name" {
   bucket = local.bucket_name
 
   tags = merge(var.tags, {
@@ -7,16 +7,16 @@ resource "aws_s3_bucket" "this" {
   })
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket                  = aws_s3_bucket.this.id
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  bucket                  = aws_s3_bucket.name.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "config" {
+  bucket = aws_s3_bucket.name.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -26,9 +26,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.this.bucket
+  value = aws_s3_bucket.name.bucket
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.this.arn
+  value = aws_s3_bucket.name.arn
 }
